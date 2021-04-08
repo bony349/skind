12cb12ac5c5dcdefd3b94e91f80590b2cd2314b7
Abdulrahman Sultan <abdosoltan349@gmail.com>
2021-04-08 19:49:54 +0200

    update covid 19 predection

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import PIL
 import numpy
-
+from keras.models import load_keras
+from skimage import transform
+import numpy as np
+from keras.preprocessing.image import load_img
 from helper import *
 from flask import Flask, request, render_template , jsonify
 
@@ -8,6 +11,13 @@ app = Flask(__name__)
 from PIL import Image
 
 svm_model = load_model('SVM.sav')
+covid =  load_keras('Covid.h5')
+def Load_Images(img):
+  pred_img = np.array(img).astype('float32')/255
+  pred_img = transform.resize(pred_img,(200,200,3))
+  pred_img = np.expand_dims(pred_img,axis=0)
+  return pred_img
+
 @app.route('/')
 def index():
     new_test = []  # new images
@@ -55,6 +65,19 @@ def post():
         return jsonify({'prediction':'Psoriasis'})
     elif (prediction == 2):
         return jsonify({'prediction':'Melanoma'})
+
+@app.route('/CovidRequest', methods=['POST'])
+def post():
+   imagefile = request.files.get('imagefile', '')
+   Image_to_pred = load_img(imagefile)
+   Image_to_pred = Load_Images(Image_to_pred)
+   prediction = np.argmax(covid.predict(Image_to_pred))
+    if (prediction == 0):
+        return jsonify({'prediction':'Non Informative Data'})
+    elif (prediction == 1):
+        return jsonify({'prediction':'Negative'})
+    elif (prediction == 2):
+        return jsonify({'prediction':'Positive'})
 
 
 if __name__ == '__main__':
